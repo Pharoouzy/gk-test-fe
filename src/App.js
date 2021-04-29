7f008cdcd3fe90ace3030149d2033ea34fab34b0
Pharoouzy <pharoouzy@yahoo.com>
2021-04-29 05:35:08 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Route, Switch } from "react-router-dom/";
 
 function App() {
   return (
-    <>
+    <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
       </Switch>
-    </>
+    </div>
   );
 }
 
