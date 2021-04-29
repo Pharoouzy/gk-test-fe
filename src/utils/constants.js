177c6f6d1ba19743a53f6dee5009b91b32b908d2
Pharoouzy <pharoouzy@yahoo.com>
2021-04-29 05:26:08 +0100

    update constants file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ export const constant = {
     process.env.NODE_ENV === "development"
       ? "http://order.test/api/v1"
       : "https://gokada-test-api.herokuapp.com/api/v1",
-  apiKey: "AIzaSyDy_0KGBQQWlCfn7JjOzXzk6eQfaF5IZS8",
+  apiKey: "AIzaSyDy_XXXXX",
   radius: 50000,
 };
